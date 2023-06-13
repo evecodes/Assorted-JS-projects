32d5ef43ea9e00854050c4c0021c46081dac0e6f
evecodes <99173892+evecodes@users.noreply.github.com>
2023-06-13 15:20:42 +0200

    JS hsl randomizer
    JS hsl randomizer.

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log('banana')
+//Randomize rgb and hsl//
+
+const boxColorRgb = document.querySelector(".box-color-rgb");
+const btnRandomizeRgb = document.querySelector("#randomize-rgb");
+const boxColorHsl = document.querySelector(".box-color-hsl");
+const btnRandomizeHsl = document.querySelector("#randomize-hsl");
+const hslTextNumber = document.querySelector(".hsl-number")
+
+//rgb//
+
+
+
+//hsl//
+function randomHSL() {
+    return "hsl" + "(" + Math.floor(Math.random()*359) + "," + Math.floor(Math.random()*99) + "%" + "," + Math.floor(Math.random()*99) + "%" + ")";
+}
+
+const hslNumber = randomHSL();
+
+function checkHSLNumber() {
+    hslTextNumber.textContent = randomHSL();
+}
+
+function startHSL() {
+    boxColorHsl.style.backgroundColor = randomHSL();
+}
+
+btnRandomizeHsl.addEventListener("click", e => {    
+    boxColorHsl.style.backgroundColor = randomHSL()
+    checkHSLNumber();
+    console.log(boxColorHsl);
+})
+
+startHSL();
+checkHSLNumber();
+// on page load a color
